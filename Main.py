06e60175a1be641c7bc2770848c960f44aace497
CAN-Felix <azadofelix@gmail.com>
2025-04-26 22:01:34 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import random
 import time
+c=random.randint(1, 2)
+z=random.randint(1, 3)
 a=random.randint(1, 9)
 def programacion1():
   b=int(input('Escribe un número del 1 al 9'))
@@ -8,6 +10,8 @@ if b==a:
   print('Que mala suerte')
 else:
   for i in range(7):
+    time.sleep(c)
     print('intent de nuevo')
+    time.sleep(z)
     programacion1()
   print('uf, que buena suerte tienes')
